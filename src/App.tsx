037551dd8103c650f147e5fe1d503c2633f8c671
Brandon Ly <wowbob396@gmail.com>
2021-03-04 07:48:38 -0600

    added threejs stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { VRCanvas, Hands, DefaultXRControllers } from '@react-three/xr';
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VRCanvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <DefaultXRControllers />
+      <Hands />
+    </VRCanvas>
   );
 }
 
